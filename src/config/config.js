1f09382f246b0fdaf386d93e524d0594fae3fb00
Abdulrazaqsodiq <Abdulrazaqsodiq2015@gmail.com>
2024-09-16 17:03:58 +0100

    he did it agian

--- src/config/config.js
+++ src/config/config.js
@@ -1,45 +1,55 @@
+/** @format */
+
 require("dotenv").config();
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_DIALECT } =
-  process.env;
+	process.env;
 
-module.exports = { 
-  development: {
-    databases: {
-      rest: {
-        database: DB_NAME,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        host: DB_HOST,
-        port: DB_PORT,
-        dialect: DB_DIALECT,
-        logging: false,
-        pool: {
-          max: 151,
-          min: 0,
-          acquire: 60000,
-          idle: 10000,
-        },
-      },
-    },
-  },
-  production: {
-    databases: {
-      rest: {
-        database: DB_NAME,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        host: DB_HOST,
-        port: DB_PORT,
-        dialect: DB_DIALECT,
-        logging: false,
-        pool: {
-          max: 151,
-          min: 0,
-          acquire: 60000,
-          idle: 10000,
-        },
-      },
-    },
-  },
+console.log({ DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_DIALECT });
+
+module.exports = {
+	development: {
+		databases: {
+			rest: {
+				database: DB_NAME,
+				username: DB_USER,
+				password: DB_PASSWORD,
+				host: DB_HOST,
+				port: DB_PORT,
+				dialect: DB_DIALECT,
+				logging: false,
+				dialectOptions: {
+					ssl: {
+						require: true,
+						rejectUnauthorized: false,
+					},
+				},
+				pool: {
+					max: 151,
+					min: 0,
+					acquire: 60000,
+					idle: 10000,
+				},
+			},
+		},
+	},
+	production: {
+		databases: {
+			rest: {
+				database: DB_NAME,
+				username: DB_USER,
+				password: DB_PASSWORD,
+				host: DB_HOST,
+				port: DB_PORT,
+				dialect: DB_DIALECT,
+				logging: false,
+				pool: {
+					max: 151,
+					min: 0,
+					acquire: 60000,
+					idle: 10000,
+				},
+			},
+		},
+	},
 };
