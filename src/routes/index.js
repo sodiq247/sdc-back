3b749de5038969cd6764d6b88f7074a909975df0
unknown <you@example.com>
2024-10-11 08:58:20 +0100

    fix bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const indexRoute = require("express").Router();
 /* GET home page. */
 // indexRoute.get("/*", verityToken);
 indexRoute.use("/api/v1/account", accountRoute);
-indexRoute.use("/api/v1", verityToken, securedRoute);
+indexRoute.use("/api/v18", verityToken, securedRoute);
 
 indexRoute.get("/", function (req, res, next) {
   res.send("welcome to sodiq app");
