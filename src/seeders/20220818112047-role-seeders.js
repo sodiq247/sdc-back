4ca64fd2f16ac5658fcef47c81e456f1996640f6
unknown <you@example.com>
2024-10-14 17:31:39 +0100

    database name

--- src/seeders/20220818112047-role-seeders.js
+++ src/seeders/20220818112047-role-seeders.js
@@ -1,20 +1,20 @@
 "use strict";
 module.exports = {
   up: async (queryInterface) => {
-    queryInterface.bulkInsert("roles", [
-      {
-        name: "superadmin",
-        description: "this is a test role",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        name: "user",
-        description: "Service Provider User",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
+    // queryInterface.bulkInsert("roles", [
+    //   {
+    //     name: "superadmin",
+    //     description: "this is a test role",
+    //     created_at: new Date(),
+    //     updated_at: new Date(),
+    //   },
+    //   {
+    //     name: "user",
+    //     description: "Service Provider User",
+    //     created_at: new Date(),
+    //     updated_at: new Date(),
+    //   },
+    // ]);
   },
   down: async (queryInterface) => {
     await queryInterface.bulkDelete("departments", {
