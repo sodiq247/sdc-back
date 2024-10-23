03785679d141d7ccb57be37cd3da93eec63dade1
Abdulrazaqsodiq <Abdulrazaqsodiq2015@gmail.com>
2024-10-23 10:44:54 +0100

    find user by email in sign up function

--- src/seeders/20220818112047-role-seeders.js
+++ src/seeders/20220818112047-role-seeders.js
@@ -1,24 +1,36 @@
 "use strict";
+const { Op } = require("sequelize");
+
 module.exports = {
   up: async (queryInterface) => {
-    queryInterface.bulkInsert("roles", [
-      {
-        name: "superadmin",
-        description: "this is a test role",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        name: "user",
-        description: "Service Provider User",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
+    // Check if the roles already exist in the database
+    const existingRoles = await queryInterface.sequelize.query(
+      `SELECT name FROM roles WHERE name IN ('superadmin', 'user')`,
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    );
+
+    // If roles don't exist, insert them
+    if (existingRoles.length === 0) {
+      await queryInterface.bulkInsert("roles", [
+        {
+          name: "superadmin",
+          description: "this is a test role",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: "user",
+          description: "Service Provider User",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ]);
+    }
   },
+
   down: async (queryInterface) => {
-    await queryInterface.bulkDelete("departments", {
-      [Op.or]: [{ name: "USD" }, { name: "EUR" }],
+    await queryInterface.bulkDelete("roles", {
+      [Op.or]: [{ name: "superadmin" }, { name: "user" }],
     });
   },
 };
